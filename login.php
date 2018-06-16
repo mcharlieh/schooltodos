08f737eae3b9d686d7b6d90ae90153aa1538a70d
Char Char <photoheart13@gmail.com>
2018-06-15 20:48:25 -0500

    login

---
+++ login.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	
+	<?php
+		include 'openConn.php';
+	
+		$user = (isset($_POST['user']) ?$_POST['user'] : '');
+		$pass = (isset($_POST['pass']) ?$_POST['pass'] : '');
+	
+		$sqlLogon = "select id from tblUSERS where loginName=$user and loginPass=$pass";
+	
+		try {
+			$conn->exec($sqlLogon);
+			echo("Login successful!");
+		}
+		catch (PDOException $f) {
+			echo ("Invalid username or password. If you forgot your password, you can reset it using the button below.");
+		}
+	
+		$conn = null;
+	
+	?>
+	
+</body>
+</html>
